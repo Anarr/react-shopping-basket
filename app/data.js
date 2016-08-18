f7d9d602d02815503420b3f9c7e166a083a58b6d
Anar <anar.rzayev94@gmail.com>
2016-08-18 04:05:27 +0400

    Add files via upload
    those are new files

---
+++ app/data.js
@@ -0,0 +1,16 @@
+module.exports = {
+	products:[
+		"computer":{
+			"name":"Pc computer",
+			"price":750
+		},
+		"mouse":{
+			"name":"Laser mouse",
+			"price":75
+		},
+		"keyboard":{
+			"name":"geniue keyboard",
+			"price":85
+		}
+	]
+}
